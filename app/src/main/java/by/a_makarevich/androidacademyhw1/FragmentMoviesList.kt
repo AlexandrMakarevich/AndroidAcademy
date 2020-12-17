f0a60da5a5d83979bea777ee346942ba66fb21e2
alexandrmakarevich <lemas096@gmail.com>
2020-12-17 16:58:54 +0300

    feat: homeWork6 complete

--- app/src/main/java/by/a_makarevich/androidacademyhw1/FragmentMoviesList.kt
+++ app/src/main/java/by/a_makarevich/androidacademyhw1/FragmentMoviesList.kt
@@ -12,11 +12,20 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import by.a_makarevich.androidacademyhw1.adapter.MovieAdapter
 import by.a_makarevich.androidacademyhw1.adapter.OnClickListenerDetail
+import by.a_makarevich.androidacademyhw1.data.Movie
+import by.a_makarevich.androidacademyhw1.data.loadMovies
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 
 class FragmentMoviesList : Fragment(), OnClickListenerDetail {
 
+    private var recyclerView: RecyclerView? = null
     private val movieAdapter = MovieAdapter(this)
+    private var movieList: List<Movie>? = null
+    private var job: Job? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,22 +34,29 @@ class FragmentMoviesList : Fragment(), OnClickListenerDetail {
     ): View? {
         Log.d("MyLog", "FragmentMoviesList_onCreateView")
         val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
+        initViews(view)
 
-        val recyclerView: RecyclerView? = view?.findViewById(R.id.recyclerView)
+        CoroutineScope(Dispatchers.Main).launch {
+            job = CoroutineScope(Dispatchers.IO).launch {
+                movieList = loadMovies(requireContext())
+                movieAdapter.setData(movieList!!)
+            }
+        }
+
         val layoutManager = recyclerView?.layoutManager as GridLayoutManager
-
-
-        recyclerView.addItemDecoration(MovieListItemDecoration(50, numberSpans(layoutManager)))
-
-
+        recyclerView?.addItemDecoration(MovieListItemDecoration(50, numberSpans(layoutManager)))
         recyclerView.apply {
-            this.adapter = movieAdapter
+            this?.adapter = movieAdapter
         }
         return view
     }
 
-    override fun onItemClick() {
-        findNavController().navigate(R.id.action_fragmentMoviesList_to_fragmentMovieDetails)
+
+    override fun onDestroyView() {
+        clearCashViews()
+        job?.cancel()
+
+        super.onDestroyView()
     }
 
     private fun numberSpans(layoutManager: GridLayoutManager): Int {
@@ -53,4 +69,18 @@ class FragmentMoviesList : Fragment(), OnClickListenerDetail {
         }
     }
 
+    private fun initViews(view: View) {
+        recyclerView = view.findViewById(R.id.recyclerView)
+    }
+
+    private fun clearCashViews() {
+        recyclerView = null
+    }
+
+    override fun onItemClick(movie: Movie) {
+        //Log.d("MyLog", movie.overview)
+        val bundle = Bundle()
+        bundle.putParcelable(Movie::class.java.toString(), movie)
+        findNavController().navigate(R.id.action_fragmentMoviesList_to_fragmentMovieDetails, bundle)
+    }
 }
